3cf3cb4776e9791313bb568c155883177e980817
nienkek2101 <nienke.kapers@gmail.com>
2021-12-15 09:11:42 +0100

    Start project

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+function HomePage() {
+    useEffect(() => {
+
+    }, []);
+    return (
+        <>
+
+        </>
+    );
+
+}
+
+export default HomePage;
